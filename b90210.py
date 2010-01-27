c0626c6b72c3891bf8fe2a81699acc20e9a86f7b
Hareesh Nagarajan <user@ubuntu.localdomain>
2010-01-27 00:34:47 -0500

    init

---
+++ b90210.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python
+#
+# Copyright 2010 Hareesh Nagarajan
+
+from django.utils import simplejson
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext import db
+from google.appengine.api import memcache
+import StringIO
+import calendar
+import datetime
+import logging
+import os
+import random
+import time
+import traceback
+import twitter
+import urllib2
+import wsgiref.handlers
+
+IGNORE =  [ 'apiwiki.twitter.com',
+            'twitterfeed.com',
+            'rss2twitter.com',
+            'skygrid.com',
+            'assetize.com',
+            'Twitter Tools',
+            'wp-to-twitter',
+            'pivotallabs.com',
+            '/devices',
+            'bit.ly',
+            'allyourtweet.com'
+            'wordpress',
+            'alexking.org',
+            'bravenewcode.com',
+            'tweethopper.com',
+            ]
+
+class SearchObject(db.Model):
+  text = db.StringProperty(multiline=True)
+  profile_image_url = db.StringProperty()
+  from_user = db.StringProperty()
+  source = db.StringProperty()
+  tweet_id = db.IntegerProperty(indexed=True)
+  created_at = db.DateTimeProperty(indexed=True)
+
+class RTLinkObject(db.Model):
+  text = db.StringProperty(multiline=True)
+  profile_image_url = db.StringProperty()
+  from_user = db.StringProperty()
+  source = db.StringProperty()
+  tweet_id = db.IntegerProperty(indexed=True)
+  created_at = db.DateTimeProperty(indexed=True)
+
+class HashtagObject(db.Model):
+  tag = db.StringProperty()
+  views = db.IntegerProperty(default=1)
+  date = db.DateTimeProperty(auto_now_add=True)
+
+class LinkObject(db.Model):
+  tag = db.StringProperty()
+  views = db.IntegerProperty(default=1)
+  date = db.DateTimeProperty(auto_now_add=True)
+
+class TwitterSearch:
+  def __init__(self, query, lang='en'):
+    self.url = 'http://search.twitter.com/search.json?q=%s&rpp=100' % query
+    self.lang = lang
+
+  def search(self):
+    try:
+      handle = urllib2.urlopen(self.url)
+      data = simplejson.loads(handle.read())
+      results = data['results']
+      self.process_results(results)
+    except Exception, e:
+      s = StringIO.StringIO()
+      traceback.print_exc(file=s)
+      logging.info('Oops: %s', s.getvalue())
+
+  def ignore_result(self, result):
+    if result.has_key('iso_language_code'):
+      if result['iso_language_code'] != self.lang:
+        return True
+
+    for ign in IGNORE:
+      if ign in result['source']:
+        return True
+
+    return False
+
+  def no_rt_or_link(self, result):
+    if (result['text'].find('http://') == -1 and
+        result['text'].find('RT ') == -1):
+      return True
+    return False
+
+  def extract_source(self, source):
+    idx = source.find('http://') + len('http://')
+    length1 = source[idx:].find('/')
+    length2 = source[idx:].find('&')
+    length = length1
+    if length2 != -1 and length2 < length1:
+      length = length2
+    just_source = source[idx:idx + length2].replace('www.', '')
+    just_source = just_source.split('/')[0]
+    return just_source
+
+  def format_tweet(self, text):
+    if text.find('@') != -1:
+      tokens = text.split()
+      formatted = []
+      for token in tokens:
+        if token.find('@') == 0:
+          at = '@<a href="http://twitter.com/%s">%s</a>' % (token[1:], token[1:])
+          formatted.append(at)
+        formatted.append(token)
+      ret = ' '.join(formatted)
+      logging.info('Formatted!! %s', ret)
+    return text
+
+  def get_date(self, created_at):
+    ts = calendar.timegm(time.strptime(created_at,
+                                       '%a, %d %b %Y %H:%M:%S +0000'))
+    return datetime.datetime.fromtimestamp(ts)
+
+  def tweet_exist(self, tweet_id):
+    result_list = db.GqlQuery(
+      "SELECT * FROM SearchObject WHERE tweet_id = :tid "
+      "ORDER BY tweet_id DESC LIMIT 1000",
+      tid=tweet_id)
+    for result in result_list:
+      return True
+
+    result_list = db.GqlQuery(
+      "SELECT * FROM RTLinkObject WHERE tweet_id = :tid "
+      "ORDER BY tweet_id DESC LIMIT 1000",
+      tid=tweet_id)
+    for result in result_list:
+      return True
+
+    return False
+
+  def process_results(self, results):
+    regular_idx = 0
+    rt_idx = 0
+    ignore = 0
+    exists = 0
+    total = 0
+    to_write = []
+    for result in results:
+      total += 1
+      if self.ignore_result(result):
+        ignore += 1
+        continue
+      
+      if self.tweet_exist(result['id']):
+        exists += 1
+        continue
+        
+      write_obj = None
+      if self.no_rt_or_link(result):
+        write_obj = SearchObject()
+        regular_idx += 1
+      else:
+        write_obj = RTLinkObject()
+        rt_idx += 1
+
+      write_obj.tweet_id = result['id']
+      write_obj.created_at = self.get_date(result['created_at'])
+      write_obj.text = result['text']
+      write_obj.profile_image_url = result['profile_image_url']
+      write_obj.from_user = result['from_user']
+      write_obj.source = self.extract_source(result['source'])
+      to_write.append(write_obj)
+
+    # Write it all in 1 shot.
+    db.put(to_write)
+          
+    logging.info('total: %d wrote [search: %d, rt: %d] ignored: %d, exists: %d' %
+                 (total, regular_idx, rt_idx, ignore, exists))
+
+
+def NO_RT_OR_LINK(result):
+  if (result.text.find('http://') == -1 and
+      result.text.find('RT ') == -1):
+    return True
+  return False
+
+# <img alt="%s" border=0 width=48px height=48px src="{{ result.profile_image_url }}">
+class Home(webapp.RequestHandler):
+  def get(self):
+    result_list = db.GqlQuery(
+      "SELECT * FROM SearchObject ORDER BY created_at DESC LIMIT 1000")
+    template_values = {
+      'result_list' : result_list,
+      }
+    path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+    self.response.out.write(template.render(path, template_values))
+    
+class Cron(webapp.RequestHandler):
+  def get(self):
+    ts = TwitterSearch('cancer')
+    ts.search()
+
+class Sources(webapp.RequestHandler):
+  def get(self):
+    ts = TwitterSearch('cancer')
+    ts.search()
+
+def main():
+  wsgiref.handlers.CGIHandler().run(webapp.WSGIApplication([
+    ('/', Home),
+    ('/cron', Cron),
+    ('/sources', Sources),
+    ]))
+   
+if __name__ == '__main__':
+  main()
